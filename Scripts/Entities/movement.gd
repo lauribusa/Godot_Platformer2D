644e07d3abc5de5a66366fb72a983a3371b82ae1
LB <lauri.busa@gmail.com>
2024-11-14 12:23:36 +0100

    added basic movement

--- Scripts/Entities/movement.gd
+++ Scripts/Entities/movement.gd
@@ -1,23 +1,27 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED:float = 300.0
+const JUMP_VELOCITY:float = -250.0
+const gravity:float = 700.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	velocity.x = 0;
+	if Input.is_key_pressed(KEY_LEFT):
+		velocity.x -= SPEED
+	if Input.is_key_pressed(KEY_RIGHT):
+		velocity.x += SPEED
+		
+	if is_on_floor() and Input.is_key_pressed(KEY_SPACE):
+			velocity.y += JUMP_VELOCITY
+	
 	move_and_slide()
+	
+	if(global_position.y > 100.0):
+		game_over()
+
+func game_over():
+	get_tree().reload_current_scene()
